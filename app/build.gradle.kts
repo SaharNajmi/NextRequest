e13e567a79488bce34e4f8d6a471af006485130e
saharnajmi <saharnajmi8089@gmail.com>
2026-02-24 09:43:17 +0100

    add client websocket dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,5 @@ dependencies {
     implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.websockets)
 }
